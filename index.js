c543e3a52032975ef445f3dd49325c628c87d3b3
Jose Beltran <55297825+jose-beltran@users.noreply.github.com>
2024-07-26 03:38:49 +0000

    Se añade responsive para widht 1300px

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerText = texto;
+    let elementoHTML = document.getElementById(elemento)
+    elementoHTML.innerText = texto
 }
 
 
 function obtenerYEncriptarValor() {
 
-    
+
 
     //Encriptar texto
-    let textoSinEncriptar = document.getElementById('input').value;
+    let textoSinEncriptar = document.getElementById('input').value
     let encriptado = textoSinEncriptar.replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    asignarTextoElemento("texto_final", encriptado);
+        .replace(/u/g, "ufat")
+    asignarTextoElemento("texto_final", encriptado)
 
     //Mostrar Botón de copiar
-    document.getElementById("container_result_buttonCopiar").style.display = "block";
-    ocultarElementos();
+    document.getElementById("container_result_buttonCopiar").style.display = "block"
+    alinearElementos("container_result")
+    ocultarElementos()
+    //document.getElementById("container_result").style.display.height = '80%'
 }
 
 function obtenerYDesencriptarValor() {
-    let textoEncriptado = document.getElementById('input').value;
+    let textoEncriptado = document.getElementById('input').value
     let textoDesencriptado = textoEncriptado
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
-        .replace(/ufat/g, 'u');
+        .replace(/ufat/g, 'u')
 
     asignarTextoElemento('texto_final', textoDesencriptado)
+    alinearElementos()
 }
 
 function copiarTexto() {
     //Copiar texto
-    let elementoTexto = document.getElementById('texto_final');
-    let texto = elementoTexto.innerText;
+    let elementoTexto = document.getElementById('texto_final')
+    let texto = elementoTexto.innerText
 
-    navigator.clipboard.writeText(texto);
+    navigator.clipboard.writeText(texto)
 }
 
 function ocultarElementos() {
-    let image = document.getElementById("container_result_img");
-    let tittle = document.getElementById("container_result_tittle");
-    let texto = document.getElementById("container_result_p");
+    let image = document.getElementById("container_result_img")
+    let tittle = document.getElementById("container_result_tittle")
+    let texto = document.getElementById("container_result_p")
     let button = document.getElementById("container_result_buttonCopiar")
 
     if (button.style.display === 'none' || button.style.display === '') {
@@ -57,4 +60,14 @@ function ocultarElementos() {
         tittle.style.display = 'none'
         texto.style.display = 'none'
     }
+}
+
+function alinearElementos(id) {
+    let container = document.getElementById(id)
+    if (container.style.height === '80%') {
+        container.style.height = 'auto'
+    } else {
+        container.style.height = '80%'
+    }
+
 }
